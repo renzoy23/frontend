7e7eee870ea73cf33888efa25f9f6088269cd707
renzoy23 <anengaguilera@gmail.com>
2023-05-14 13:42:36 +0800

    Added Axios Functionality to acces OpenAI

---
+++ renderer/js/renderer.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form_sentence");
+if (form) {
+    form.onsubmit = async function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+
+        // console.log(formData.get("sentence"));
+
+        const response = await window.axios.openAI(formData.get("sentence"));
+        document.getElementById("sentence_corrected").innerHTML = JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
+    };
+}
